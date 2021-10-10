9a875ad3ad0aca7ea952973b8adba93bd72b92a1
mistery <cloud2037@gmail.com>
2021-10-11 01:44:43 +0800

    rewrite init_shell_vimrc.sh

--- init_shell_vimrc.sh
+++ init_shell_vimrc.sh
@@ -1,3 +1,17 @@
-# use `source FILENAME` execute this
-source ./vimrc-cfg/init.sh
-source ./zsh-bash-cfg/init.sh
+#!/bin/bash
+CPWD=$(cd $(dirname $0) && pwd)
+if [[ -x "${CPWD}"/vimrc-cfg/init.sh ]]; then 
+  "${CPWD}"/vimrc-cfg/init.sh
+else
+  chmod +x "${CPWD}"/vimrc-cfg/init.sh
+  "${CPWD}"/vimrc-cfg/init.sh
+  chmod -x "${CPWD}"/vimrc-cfg/init.sh
+fi
+
+if [[ -x "${CPWD}"/zsh-bash-cfg/init.sh ]]; then
+  "${CPWD}"/zsh-bash-cfg/init.sh
+else
+  chmod +x "${CPWD}"/zsh-bash-cfg/init.sh
+  "${CPWD}"/zsh-bash-cfg/init.sh
+  chmod -x "${CPWD}"/zsh-bash-cfg/init.sh
+fi
