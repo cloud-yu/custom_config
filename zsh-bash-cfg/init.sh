5063c2676f78899234273767461db2232f4fd85d
Mistery <cloud2037@gmail.com>
2021-12-17 15:21:45 +0800

    add a login script auto create a tmux-session if the server do not has one

--- zsh-bash-cfg/init.sh
+++ zsh-bash-cfg/init.sh
@@ -1,12 +1,22 @@
-#!/bin/zsh
+#!/bin/bash
 SCRIPT_PWD=$(cd "$(dirname "$0")" && pwd)
 
 echo "${SCRIPT_PWD}"
 
 # copy profile to $HOME
-if [ -n "${BASH_VERSION}" ]; then
+
+
+if [[ -n "${BASH_VERSION}" ]]; then
     cp "${SCRIPT_PWD}"/custom.profile "${HOME}"/.profile
-else
-    cp "${SCRIPT_PWD}"/custom.profile "${HOME}"/.zprofile
-    cp "${SCRIPT_PWD}"/*.zshrc "${HOME}"/.zshrc
+fi
+
+if [[ -x $(which zsh) ]]; then
+    if [[ -e "${SCRIPT_PWD}"/zinit_custom.zshrc ]]; then
+        cp "${SCRIPT_PWD}"/custom.profile "${HOME}"/.zprofile
+        cp "${SCRIPT_PWD}"/zinit_custom.zshrc "${HOME}"/.zshrc
+    else
+        cp "${SCRIPT_PWD}"/custom.profile "${HOME}"/.zprofile
+        cp "${SCRIPT_PWD}"/antigen_custom.zshrc "${HOME}"/.zshrc
+
+    fi
 fi
