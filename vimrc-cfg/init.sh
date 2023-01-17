6bd886a0e0035a22f92d6ea07131c7ccc1c66730
mistery <cloud2037@gmail.com>
2023-01-17 17:09:02 +0800

    change: change some configuration

--- vimrc-cfg/init.sh
+++ vimrc-cfg/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SCRIPT_PWD=$(cd "$(dirname "$0")" && pwd)
 VER_INFO=$(vim --version | head -1)
-if [[ ${VER_INFO} =~ [[:alpha:][:blank:]\-]([[:digit]]\.[[:digit:]]).+ ]]; then
-    if (( $match < 9 )); then
+if [[ ${VER_INFO} =~ [[:alpha:][:blank:]\-]+([[:digit:]])\.[[:digit:]].+ ]]; then
+    if (( ${BASH_REMATCH[-1]} < 9 )); then
         ln -fs "${SCRIPT_PWD}"/vimrc "${HOME}"/.vimrc
     else
         ln -fs "${SCRIPT_PWD}/vimrc-9" "${HOME}/.vimrc"
