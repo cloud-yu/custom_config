d2129ba4c383d8537047b9e2e34df1b4b8658efa
mistery <cloud2037@gmail.com>
2023-01-17 10:59:33 +0800

    1. fix: SetTitle auto goto last line after add title 2. add: init.sh support vim9script vimrc file for vim version above 9.0

--- vimrc-cfg/init.sh
+++ vimrc-cfg/init.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 SCRIPT_PWD=$(cd "$(dirname "$0")" && pwd)
-ln -fs "${SCRIPT_PWD}"/vimrc "${HOME}"/.vimrc
+VER_INFO=$(vim --version | head -1)
+if [[ ${VER_INFO} =~ [[:alpha:][:blank:]\-]([[:digit]]\.[[:digit:]]).+ ]]; then
+    if (( $match < 9 )); then
+        ln -fs "${SCRIPT_PWD}"/vimrc "${HOME}"/.vimrc
+    else
+        ln -fs "${SCRIPT_PWD}/vimrc-9" "${HOME}/.vimrc"
+    fi
+else
+    echo "no vim installed, use legacy vimrc"
+    ln -fs "${SCRIPT_PWD}/vimrc" "${HOME}/.vimrc"
+fi
